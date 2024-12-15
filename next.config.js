e59b97cb0ed557635a8dd4c2d9eb2fcc1945c796
karthik-py-dev <157446477+karthik-py-dev@users.noreply.github.com>
2024-12-15 20:31:03 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,  // Ensure this is enabled if you're using the app directory
   },
 };
 
